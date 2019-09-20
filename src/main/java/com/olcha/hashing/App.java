dcf7ce2cdc334ec5da4f40a3c1e34be5ed09bbdd
olga0904 <olga.chaula@hotmail.com>
2019-09-20 14:28:40 +0200

    first commit

---
+++ src/main/java/com/olcha/hashing/App.java
@@ -0,0 +1,13 @@
+package com.olcha.hashing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
